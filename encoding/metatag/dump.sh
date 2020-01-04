676c2248dc5fa31a1b9f624d5c84ea0a7aa569e9
SetraR <setrar@valiha.com>
2020-01-03 23:01:14 -0500

    .

--- encoding/metatag/dump.sh
+++ encoding/metatag/dump.sh
@@ -8,5 +8,5 @@ TIAr0000000196Al0000000001So0000006243.mp3
 
 for FILE in ${FILES}
 do
-   ffprobe -v quiet -print_format json -show_format -show_streams ${LOCATION}/${FILE}
+   ffprobe -hide_banner -loglevel fatal -show_error -show_format -show_streams -show_programs -show_chapters -show_private_data -print_format json ${LOCATION}/${FILE}
 done

32c3c077dbd8a751d631c74e9747a462200a22bb
setrar <setrar@valiha.com>
2020-01-10 12:08:12 -0500

    Create dump_dir.sh

---
+++ encoding/metatag/dump_dir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOCATION=/e/theindie.biz//shared/mp3_user/repository
+
+
+FILES=($LOCATION/*)
+
+for FILE in ${FILES[@]}
+do
+   # echo $FILE
+   ffprobe -hide_banner -loglevel fatal -show_error -show_format -show_streams -print_format json ${FILE}
+done

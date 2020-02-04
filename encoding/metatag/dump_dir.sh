afdbc30d716526c25d3e42e1bb57f669dfef8972
setrar <setrar@valiha.com>
2020-02-03 21:48:31 -0500

    Update dump_dir.sh

--- encoding/metatag/dump_dir.sh
+++ encoding/metatag/dump_dir.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-LOCATION=/e/theindie.biz/shared/mp3_user/repository
+LOCATION=/mnt/shared/theindie.biz/shared/mp3_user/repository
 
 
 FILES=($LOCATION/*)
+
+echo "["
 
 for FILE in ${FILES[@]}
 do
    # echo $FILE
    ffprobe -hide_banner -loglevel fatal -show_error -show_format -show_streams -print_format json ${FILE}
+   echo ","
 done
+echo "]"
+
+# Manually remove the last comma before the last bracket indicating the end of the array
